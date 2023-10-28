f490b1aa0791172fc1dcc25dc27c4ec3731e734e
x0ba <dax@omg.lol>
2023-10-28 15:43:02 -0700

    added uses page

--- src/content/post/skiffaliases/index.md
+++ src/content/post/skiffaliases/index.md
@@ -1,8 +1,8 @@
 ---
-title: "Skiff just released email aliases."
-publishDate: "28 Oct 2023"
-description: "It's now officially better than Protonmail."
-tags: ["nix", "linux"]
+title: "Skiff just released email aliases"
+publishDate: "28 October 2023"
+description: "This just might be the feature that gives Skiff an edge over its competitors."
+tags: ["email", "privacy"]
 ---
 
 For those of you who don't know, Skiff is
--- src/site.config.ts
+++ src/site.config.ts
@@ -1,41 +1,45 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Daniel Xu",
-	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "x0ba's space",
-	// Meta property used as the default description meta property
-	description: "My own cozy spot on the internet.",
-	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-US",
-	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_US",
-	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
-	date: {
-		locale: "en-US",
-		options: {
-			day: "numeric",
-			month: "short",
-			year: "numeric",
-		},
-	},
-	// Include view-transitions: https://docs.astro.build/en/guides/view-transitions/
-	includeViewTransitions: false,
+  // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+  author: "Daniel Xu",
+  // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+  title: "x0ba's space",
+  // Meta property used as the default description meta property
+  description: "My own cozy spot on the internet.",
+  // HTML lang property, found in src/layouts/Base.astro L:18
+  lang: "en-US",
+  // Meta property, found in src/components/BaseHead.astro L:42
+  ogLocale: "en_US",
+  // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+  date: {
+    locale: "en-US",
+    options: {
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    },
+  },
+  // Include view-transitions: https://docs.astro.build/en/guides/view-transitions/
+  includeViewTransitions: false,
 };
 
 // Used to generate links in both the Header & Footer.
 export const menuLinks: Array<{ title: string; path: string }> = [
-	{
-		title: "Home",
-		path: "/",
-	},
-	{
-		title: "About",
-		path: "/about/",
-	},
-	{
-		title: "Blog",
-		path: "/posts/",
-	},
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about/",
+  },
+  {
+    title: "Uses",
+    path: "/uses/",
+  },
+  {
+    title: "Blog",
+    path: "/posts/",
+  },
 ];
